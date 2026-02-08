dec5c3317e7b4e219c71ce08f4ed26510cfe12d0
Jesus E <jesusmix8@icloud.com>
2026-02-07 18:18:09 -0600

    Valentine page

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { useState, useRef } from "react";
+import "./App.css";
+
+function App() {
+  const [yesSize, setYesSize] = useState(1);
+  const [accepted, setAccepted] = useState(false);
+  const [noPosition, setNoPosition] = useState({ top: "60%", left: "50%" });
+
+  const audioRef = useRef(null);
+
+  const moveNoButton = () => {
+    const maxX = window.innerWidth - 120;
+    const maxY = window.innerHeight - 60;
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    setNoPosition({
+      left: `${randomX}px`,
+      top: `${randomY}px`
+    });
+
+    setYesSize(yesSize + 0.2);
+  };
+
+  const handleYes = () => {
+    const audio = audioRef.current;
+    if (audio) {
+      audio.currentTime = 0;
+      audio.play().catch(() => {});
+    }
+    setAccepted(true);
+  };
+
+
+
+  if (accepted) {
+  return (
+    <div className="container">
+
+      <p className="message">
+        Sabía que dirías que sí 🥰<br />
+        Te veo 13 de febrero 5:00 pm💘
+      </p>
+
+      {/* Imagen que aparece abajo */}
+      <img
+        src="/foto.png"
+        alt="Nosotros 💖"
+        className="photo yes-photo"
+      />
+
+    </div>
+  );
+}
+
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        ¿Quieres ser mi<br /> Valentine? 💘
+      </h1>
+
+      <button
+        className="yes"
+        style={{ transform: `scale(${yesSize})` }}
+        onClick={handleYes}
+      >
+        Sí 💖
+      </button>
+
+      <button
+        className="no"
+        style={noPosition}
+        onTouchStart={moveNoButton}
+        onMouseEnter={moveNoButton}
+      >
+        No 💔
+      </button>
+
+      {/* Audio oculto */}
+      <audio ref={audioRef} src="/love.mp3" preload="auto" />
+    </div>
+  );
+}
+
+export default App;
